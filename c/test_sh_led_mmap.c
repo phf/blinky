7b198565a9277f9ea80c6798edd90e099e95266c
Peter Froehlich <peter.hans.froehlich@gmail.com>
2018-04-12 20:01:53 -0400

    Test sync() versus sleep().

--- c/test_sh_led_mmap.c
+++ c/test_sh_led_mmap.c
@@ -4,17 +4,29 @@
 #include <string.h>
 #include <time.h>
 
+#define SECOND (1000 * 1000 * 1000)
+
 int main(void)
 {
-	struct timespec delay = {0, 4040000};
+	/* without sync() RPi 2 can do at most 81 before LEDs stall */
+	struct timespec delay = {0, (long) (SECOND / 30)};
 
 	uint16_t *leds = sh_led_mmap_open();
 	if (leds == NULL) {
 		exit(1);
 	}
 
-	for (int i = 0; i < 256; i++) {
-		memset(leds, 0xff, SH_LED_MMAP_SIZE);
+	/* flash using sync() */
+	for (int i = 0; i < 15; i++) {
+		memset(leds, 0xFF, SH_LED_MMAP_SIZE);
+		sh_led_mmap_sync();
+		memset(leds, 0x00, SH_LED_MMAP_SIZE);
+		sh_led_mmap_sync();
+	}
+
+	/* flash using sleep() */
+	for (int i = 0; i < 15; i++) {
+		memset(leds, 0xFF, SH_LED_MMAP_SIZE);
 		nanosleep(&delay, NULL);
 		memset(leds, 0x00, SH_LED_MMAP_SIZE);
 		nanosleep(&delay, NULL);
